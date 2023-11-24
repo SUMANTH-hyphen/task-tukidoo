a011618d742b94f79cb0401a6c695a9964a45cda
sumnth <20BQ1A0582@vvit.net>
2023-11-24 18:50:51 +0530

    v1.2 changes in responsive padding

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -61,13 +61,13 @@ const Home = () => {
                     </div>
                     <div className={`${currentTab !== 3 ? "hidden" : ""}`} id="segment-3" role="tabpanel" aria-labelledby="segment-item-3">
 
-                        <div id="a-body" className="flex flex-col-reverse gap-3 h-max md:flex md:flex-row">
-                            <div id="b-cam" className="w-full gap-24 flex flex-col md:w-3/12 md:gap-5">
-                                <div id="c-cam" className="flex flex-col gap-20 md:flex md:flex-col md:gap-10">
+                        <div id="a-body" className="flex flex-col-reverse gap-10 h-max md:flex md:flex-row">
+                            <div id="b-cam" className="w-full gap-20 flex flex-col md:w-3/12 md:gap-8">
+                                <div id="c-cam" className="flex gap-2 md:flex md:flex-col md:gap-10">
                                     <VideoCard src="a-little-girl-sitting-at-a-table-with-a-laptop-w9i7wMaM3EE" name="Ridha" />
                                     <VideoCard src="boy-in-gray-shirt-using-black-laptop-computer-6MePtA9EVDA" name="Mr. Kaur" />
                                 </div>
-                                <div id="c-controls" className="grid gap-y-2 px-10 md:grid-cols-4 grid-rows-2 md:gap-y-5 md:px-5">
+                                <div id="c-controls" className="grid gap-y-2 px-10 md:grid-cols-4 grid-rows-2 md:gap-y-3 md:px-3">
                                     <div >
                                         <SVGCam />
                                     </div>
@@ -80,8 +80,8 @@ const Home = () => {
                                     <div >
                                         <SVGShare />
                                     </div>
-                                    <div className=" col-start-2 col-span-2">
-                                        <button type="button" className="py-3 px-7 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-50 text-black hover:bg-blue-100 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                    <div className=" col-start-1 col-span-4">
+                                        <button type="button" className="py-3 px-16 md:px-24 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-50 text-black hover:bg-blue-100 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                             Leave
                                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
                                                 <path d="M 11.5 6 C 8.4802259 6 6 8.4802259 6 11.5 L 6 36.5 C 6 39.519774 8.4802259 42 11.5 42 L 29.5 42 C 32.519774 42 35 39.519774 35 36.5 A 1.50015 1.50015 0 1 0 32 36.5 C 32 37.898226 30.898226 39 29.5 39 L 11.5 39 C 10.101774 39 9 37.898226 9 36.5 L 9 11.5 C 9 10.101774 10.101774 9 11.5 9 L 29.5 9 C 30.898226 9 32 10.101774 32 11.5 A 1.50015 1.50015 0 1 0 35 11.5 C 35 8.4802259 32.519774 6 29.5 6 L 11.5 6 z M 33.484375 15.484375 A 1.50015 1.50015 0 0 0 32.439453 18.060547 L 36.878906 22.5 L 15.5 22.5 A 1.50015 1.50015 0 1 0 15.5 25.5 L 36.878906 25.5 L 32.439453 29.939453 A 1.50015 1.50015 0 1 0 34.560547 32.060547 L 41.560547 25.060547 A 1.50015 1.50015 0 0 0 41.560547 22.939453 L 34.560547 15.939453 A 1.50015 1.50015 0 0 0 33.484375 15.484375 z"></path>
