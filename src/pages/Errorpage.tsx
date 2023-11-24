b9bd9a009a1acb3c3c450b9c99e60fe3ce1415ea
sumnth <20BQ1A0582@vvit.net>
2023-11-24 17:17:22 +0530

    v1.1

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -1,7 +1,7 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError() ;
+  // const error = useRouteError() ;
 
   const navigate = useNavigate()
 
